5171a7fcbbe97193994633aaf8fad9a5641c1966
Akmulla <d4n73@yandex.ru>
2023-09-24 22:36:43 +0300

    iterate

--- Scripts/Economy.gd
+++ Scripts/Economy.gd
@@ -1,14 +1,14 @@
 extends Node
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var actors = get_tree().get_nodes_in_group("actors")
+	for actor in actors:
+		print_debug(actor)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var char = get_node("/root/Main/Characters/Character")
-	print_debug(char)
+	
 	pass
